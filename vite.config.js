a26091f1002496e79f5461a86e4a0d0808345442
zhangkc <zhangkc@fadada.com>
2024-07-17 18:35:33 +0800

    build(构建): 自动化

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/tom-json',
+  base: '/tom-json/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
